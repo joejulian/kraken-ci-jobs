cfdc0b28d11fbba6a2a9ef957ebc7d158b342a22
Joe Julian <me@joejulian.name>
2017-02-23 15:12:45 -0800

    Set BUILD_TAG in container so ansible will be verbose
    k2 is configured to add `-vvv` to ansible if BUILD_TAG is set. BUILD_TAG
    comes from Jenkins and will only normally be set if this is run as a
    Jenkins job.

--- scripts/include-raw001-k2-build-cluster-aws.sh
+++ scripts/include-raw001-k2-build-cluster-aws.sh
@@ -10,6 +10,7 @@ docker run -e "AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" \
   -e "AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" \
   -e "K2_CLUSTER_NAME=${K2_CLUSTER_NAME}" \
   -e "K2_KEY_LOCATION=${K2_KEY_LOCATION}" \
+  -e "BUILD_TAG=${BUILD_TAG}" \
   --volumes-from=jenkins \
   ${K2_CONTAINER_IMAGE} \
   /kraken/up.sh --output ${WORKSPACE}/${K2_CLUSTER_NAME} --config ${WORKSPACE}/${K2_CLUSTER_NAME}/${K2_CLUSTER_NAME}.yaml
--- scripts/include-raw001-k2-destroy-gke-cluster.sh
+++ scripts/include-raw001-k2-destroy-gke-cluster.sh
@@ -2,6 +2,7 @@
 docker run -e "K2_CLUSTER_NAME=${K2_CLUSTER_NAME}" \
   -e "GCE_SERVICE_ACCOUNT_ID=${KRAKEN_GCE_SERVICE_ACCOUNT_ID}" \
   -e "GCE_SERVICE_ACCOUNT_KEY='/gcloud/service-account.json'" \
+  -e "BUILD_TAG=${BUILD_TAG}" \
   --volumes-from=jenkins \
   -v /gcloud:/gcloud \
   ${K2_CONTAINER_IMAGE} \
